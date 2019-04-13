453bfc9b90f65c5c3e8380c3b2661946d9a52a46
billyTsag <billy.tsag@gmail.com>
2019-04-14 02:08:07 +0300

    Market Cap, Button Add Transaction

---
+++ CryptoFolio/CryptoFolio/Migrations/201904132231518_more coins.cs
@@ -0,0 +1,16 @@
+namespace CryptoFolio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class morecoins : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- CryptoFolio/CryptoFolio/Migrations/Configuration.cs
+++ CryptoFolio/CryptoFolio/Migrations/Configuration.cs
@@ -16,17 +16,27 @@ namespace CryptoFolio.Migrations
         protected override void Seed(CryptoFolio.Models.ApplicationDbContext context)
         {
             context.Coins.AddOrUpdate(x => x.Name,
-                new Coin() { Name = "Bitcoin", Price = 3046.25m, Symbol = "btc", Supply = 17000000m },
-                new Coin() { Name = "Ethereum", Price = 3056.25m, Symbol = "btbjnc", Supply = 17000000m },
-                new Coin() { Name = "Kostas", Price = 3096.25m, Symbol = "jh", Supply = 17000000m },
-                new Coin() { Name = "Nikos", Price = 3856.25m, Symbol = "cv", Supply = 17000000m },
-                new Coin() { Name = "Petros", Price = 4046.25m, Symbol = "dfg", Supply = 17000000m },
-                new Coin() { Name = "Makius", Price = 52246.25m, Symbol = "rtghj", Supply = 17000000m },
-                new Coin() { Name = "Billy", Price = 3089.25m, Symbol = "ert", Supply = 17000000m },
-                new Coin() { Name = "Gamiesai", Price = 3046.25m, Symbol = "sxcv", Supply = 17000000m },
-                new Coin() { Name = "Gamiemai", Price = 3046.25m, Symbol = "wert", Supply = 17000000m },
-                new Coin() { Name = "Gamietai", Price = 3046.25m, Symbol = "bnm", Supply = 17000000m }
-                );
+                new Coin() { Name = "Bitcoin", Price = 5089.25m, Symbol = "BTC", Supply = 17664112m },
+                new Coin() { Name = "Ethereum", Price = 164.35m, Symbol = "ETH", Supply = 105646790m },
+                new Coin() { Name = "Ripple", Price = 0.35m, Symbol = "XRP", Supply = 410000000m },
+                new Coin() { Name = "Bitcoin Cash", Price = 276.25m, Symbol = "BCH", Supply = 17000000m },
+                new Coin() { Name = "EOS", Price = 5.35m, Symbol = "EOS", Supply = 17000000m },
+                new Coin() { Name = "Litecoin", Price = 78.25m, Symbol = "LTC", Supply = 17000000m },
+                new Coin() { Name = "Binance Coin", Price = 18.25m, Symbol = "BNB", Supply = 17000000m },
+                new Coin() { Name = "Tether", Price = 1.01m, Symbol = "USDT", Supply = 17000000m },
+                new Coin() { Name = "Stellar", Price = 0.25m, Symbol = "XLM", Supply = 17000000m },
+                new Coin() { Name = "TRON", Price = 0.025m, Symbol = "TRX", Supply = 17000000m },
+                new Coin() { Name = "Monero", Price = 65.85m, Symbol = "XMR", Supply = 17000000m },
+                new Coin() { Name = "Dash", Price = 119.85m, Symbol = "DASH", Supply = 17000000m },
+                new Coin() { Name = "IOTA", Price = 0.35m, Symbol = "MIOTA", Supply = 17000000m },
+                new Coin() { Name = "NEO", Price = 11.5m, Symbol = "NEO", Supply = 17000000m },
+                new Coin() { Name = "Ethereum Classic", Price = 6.85m, Symbol = "ETC", Supply = 17000000m },
+                new Coin() { Name = "Ontology", Price = 1.35m, Symbol = "ONT", Supply = 17000000m },
+                new Coin() { Name = "Tezos", Price = 0.98m, Symbol = "XTZ", Supply = 17000000m },
+                new Coin() { Name = "Maker", Price = 636.1m, Symbol = "MKR", Supply = 17000000m },
+                new Coin() { Name = "NEM", Price = 0.065m, Symbol = "XEM", Supply = 17000000m },
+                new Coin() { Name = "Zcash", Price = 69.85m, Symbol = "ZEC", Supply = 17000000m }
+                );            
             context.SaveChanges();
             
         }
