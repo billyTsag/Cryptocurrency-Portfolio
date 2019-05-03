86221dea6dbc97cd57d581769c4abe38eddd6feb
billyTsag <billy.tsag@gmail.com>
2019-05-03 16:17:41 +0300

    added Events

---
+++ CryptoFolio/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoFolio.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult GetEvents()
+        {
+            return View();
+        }
+    }
+}
