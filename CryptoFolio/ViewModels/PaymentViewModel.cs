7affce1329056035bf1fc330d7a3309b00b36a41
kenza15 <kerantzakis@gmail.com>
2019-05-04 18:36:29 +0300

    Payment View

--- CryptoFolio/ViewModels/PaymentViewModel.cs
+++ CryptoFolio/ViewModels/PaymentViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace CryptoFolio.ViewModels
 {
+    
     public class PaymentViewModel
     {
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [DataType(DataType.CreditCard)]
         public int CardNumber { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Month { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Year { get; set; }
         public int Cvc { get; set; }
     }
