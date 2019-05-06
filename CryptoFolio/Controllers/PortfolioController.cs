8685d6dc4d82ab8991acf1909cabe17dc78ab332
billyTsag <billy.tsag@gmail.com>
2019-05-06 13:32:05 +0300

    more filters

--- CryptoFolio/Controllers/PortfolioController.cs
+++ CryptoFolio/Controllers/PortfolioController.cs
@@ -35,14 +35,6 @@ namespace CryptoFolio.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        //[Authorize]
-        //[HttpGet]
-        //public ActionResult ViewAllPortfolios()
-        //{
-        //    var portfolios = _context.Portfolios.ToList();
-        //    return View(portfolios);
-        //}
-
         [Authorize]
         [HttpGet]
         public ActionResult ViewMyPortfolios(string sortOrder)
@@ -109,54 +101,12 @@ namespace CryptoFolio.Controllers
             return RedirectToAction("ViewMyPortfolios", "Portfolio");
         }
 
-        //[Authorize]
-        //[HttpGet]
-        //public ActionResult ViewMyPortfolio(int PortfolioID)
-        //{
-            
-        //    var userID = User.Identity.GetUserId();
-
-        //    var coinss = _context.Transactions
-        //        .Join(_context.PortfolioCoins, t => t.TransactionID, pc => pc.TransactionID, (t, pc) => new { t, pc })
-        //        .Join(_context.Portfolios, tpc => tpc.pc.PortfolioID, port => port.PortfolioID, (tpc, port) => new { tpc, port })
-        //        .Join(_context.Coins, pc => pc.tpc.pc.CoinID, c => c.CoinID, (pc, c) => new { pc, c })
-        //        .Where(tpc => tpc.pc.port.UserID == userID && tpc.pc.port.PortfolioID == PortfolioID)
-                
-        //        .Select(tpcport =>  new MyTransactionsViewModel
-        //        {
-        //            CoinID = tpcport.pc.tpc.pc.CoinID,
-        //            Quantity = tpcport.pc.tpc.t.Quantity,
-        //            CoinName = tpcport.pc.tpc.pc.Coin.Name,
-        //            CoinPrice = tpcport.c.Price,
-        //            TransactionID = tpcport.pc.tpc.t.TransactionID,
-        //            Buy = tpcport.pc.tpc.t.Buy
-        //        }).ToList();
-           
-        //    var result = coinss.GroupBy(x => x.CoinID).Select(i => new MyTransactionsViewModel
-        //    {
-        //        CoinID = i.Key,
-        //        Quantity = i.Sum(j=>j.Quantity),
-        //        CoinName = i.First().CoinName,
-        //        CoinPrice = i.First().CoinPrice
-        //    });
-
-            
-           
-        //    return View(result);
-        //}
-
         [Authorize]
         [HttpGet]
-        public ActionResult ViewMyPortfolio(  int PortfolioID, string sortOrder)
+        public ActionResult ViewMyPortfolio(int PortfolioID)
         {
-            
-            ViewBag.CoinNameSortParm = String.IsNullOrEmpty(sortOrder) ? "CoinName_desc" : "";
-            ViewBag.QuantitySortParm = sortOrder == "Quantity" ? "quantity_desc" : "Quantity";
-            ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
-            ViewBag.ValueSortParm = sortOrder == "Value" ? "value_desc" : "Value";
-            var userID = User.Identity.GetUserId();
 
-            
+            var userID = User.Identity.GetUserId();
 
             var coinss = _context.Transactions
                 .Join(_context.PortfolioCoins, t => t.TransactionID, pc => pc.TransactionID, (t, pc) => new { t, pc })
@@ -168,8 +118,7 @@ namespace CryptoFolio.Controllers
                 {
                     CoinID = tpcport.pc.tpc.pc.CoinID,
                     Quantity = tpcport.pc.tpc.t.Quantity,
-                    CoinName = tpcport.pc.tpc.pc.Coin.Name,
-                    
+                    CoinName = tpcport.pc.tpc.pc.Coin.Name,                    
                     TransactionID = tpcport.pc.tpc.t.TransactionID,
                     Buy = tpcport.pc.tpc.t.Buy
                 }).ToList();
@@ -184,37 +133,79 @@ namespace CryptoFolio.Controllers
 
 
 
-            switch (sortOrder)
-            {
-                case "CoinName_desc":
-                    result = result.OrderByDescending(x => x.CoinName);
-                    
-                    break;
-                case "Quantity":
-                    result = result.OrderBy(x => x.DateOfPurchase);
-                    break;
-                case "quantity_desc":
-                    result = result.OrderByDescending(x => x.DateOfPurchase);
-                    break;
-                case "Price":
-                    result = result.OrderBy(x => x.CoinPrice);
-                    break;
-                case "price_desc":
-                    result = result.OrderByDescending(x => x.CoinPrice);
-                    break;
-                case "Value":
-                    result = result.OrderBy(x => x.CoinPrice * x.Quantity);
-                    break;
-                case "value_desc":
-                    result = result.OrderByDescending(x => x.CoinPrice * x.Quantity);
-                    break;
-                default:
-                    result = result.OrderBy(x => x.CoinName);
-                    break;
-            }
-
-            return View(result.ToList());
+            return View(result);
         }
+
+        //[Authorize]
+        //[HttpGet]
+        //public ActionResult ViewMyPortfolio(  int PortfolioID, string sortOrder)
+        //{
+
+        //    ViewBag.CoinNameSortParm = String.IsNullOrEmpty(sortOrder) ? "CoinName_desc" : "";
+        //    ViewBag.QuantitySortParm = sortOrder == "Quantity" ? "quantity_desc" : "Quantity";
+        //    ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
+        //    ViewBag.ValueSortParm = sortOrder == "Value" ? "value_desc" : "Value";
+        //    var userID = User.Identity.GetUserId();
+
+
+
+        //    var coinss = _context.Transactions
+        //        .Join(_context.PortfolioCoins, t => t.TransactionID, pc => pc.TransactionID, (t, pc) => new { t, pc })
+        //        .Join(_context.Portfolios, tpc => tpc.pc.PortfolioID, port => port.PortfolioID, (tpc, port) => new { tpc, port })
+        //        .Join(_context.Coins, pc => pc.tpc.pc.CoinID, c => c.CoinID, (pc, c) => new { pc, c })
+        //        .Where(tpc => tpc.pc.port.UserID == userID && tpc.pc.port.PortfolioID == PortfolioID)
+
+        //        .Select(tpcport => new MyTransactionsViewModel
+        //        {
+        //            CoinID = tpcport.pc.tpc.pc.CoinID,
+        //            Quantity = tpcport.pc.tpc.t.Quantity,
+        //            CoinName = tpcport.pc.tpc.pc.Coin.Name,
+
+        //            TransactionID = tpcport.pc.tpc.t.TransactionID,
+        //            Buy = tpcport.pc.tpc.t.Buy
+        //        }).ToList();
+
+        //    var result = coinss.GroupBy(x => x.CoinID).Select(i => new MyTransactionsViewModel
+        //    {
+        //        CoinID = i.Key,
+        //        Quantity = i.Sum(j => j.Quantity),
+        //        CoinName = i.First().CoinName,
+        //        CoinPrice = i.First().CoinPrice
+        //    });
+
+
+
+        //    switch (sortOrder)
+        //    {
+        //        case "CoinName_desc":
+        //            result = result.OrderByDescending(x => x.CoinName);
+
+        //            break;
+        //        case "Quantity":
+        //            result = result.OrderBy(x => x.DateOfPurchase);
+        //            break;
+        //        case "quantity_desc":
+        //            result = result.OrderByDescending(x => x.DateOfPurchase);
+        //            break;
+        //        case "Price":
+        //            result = result.OrderBy(x => x.CoinPrice);
+        //            break;
+        //        case "price_desc":
+        //            result = result.OrderByDescending(x => x.CoinPrice);
+        //            break;
+        //        case "Value":
+        //            result = result.OrderBy(x => x.CoinPrice * x.Quantity);
+        //            break;
+        //        case "value_desc":
+        //            result = result.OrderByDescending(x => x.CoinPrice * x.Quantity);
+        //            break;
+        //        default:
+        //            result = result.OrderBy(x => x.CoinName);
+        //            break;
+        //    }
+
+        //    return View(result.ToList());
+        //}
 
 
     }
